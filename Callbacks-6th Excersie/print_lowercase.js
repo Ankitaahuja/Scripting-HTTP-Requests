8edfb314a3ff308ea23769edfcafb40196d79a14
ankitaahuja111 <ankitaahuja111@gmail.com>
2018-09-03 18:46:52 +0000

    step 6 complete

---
+++ Callbacks-6th Excersie/print_lowercase.js
@@ -0,0 +1,15 @@
+var https = require('https');
+var getHTML = require('../module-5thStep');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/uppercase.html'
+};
+
+function printUpperCase (html) {
+    html = html.toLowerCase();
+    console.log(html);
+
+}
+
+getHTML(requestOptions, printUpperCase);
